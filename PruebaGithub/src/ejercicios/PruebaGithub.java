1534a52368d1c5580c14886e9cd822371df61810
MRL <munozredondolucas@gmail.com>
2026-01-09 10:04:24 +0100

    syso hola master2

--- PruebaGithub/src/ejercicios/PruebaGithub.java
+++ PruebaGithub/src/ejercicios/PruebaGithub.java
@@ -5,6 +5,7 @@ public class PruebaGithub {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("esto es una prueba de GitHub");
+		System.out.println("Hola");
 
 	}
 
